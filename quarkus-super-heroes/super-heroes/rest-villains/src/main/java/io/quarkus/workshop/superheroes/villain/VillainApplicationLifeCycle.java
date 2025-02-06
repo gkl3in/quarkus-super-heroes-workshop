269b0384c4406710ca3a8c047ecaa11babecf545
Gabriel Klein <gabrielklein289@hotmail.com>
2025-02-06 09:58:23 -0300

    feat: added banner on startup

---
+++ quarkus-super-heroes/super-heroes/rest-villains/src/main/java/io/quarkus/workshop/superheroes/villain/VillainApplicationLifeCycle.java
@@ -0,0 +1,27 @@
+package io.quarkus.workshop.superheroes.villain;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import io.quarkus.runtime.configuration.ConfigUtils;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class VillainApplicationLifeCycle {
+
+    private static final Logger LOGGER = Logger.getLogger(VillainApplicationLifeCycle.class);
+
+    void onStart(@Observes StartupEvent ev) {
+        LOGGER.info("   _  ___      _                    _____ _____ ");
+        LOGGER.info("  | |/ / |    (_)             /\\   |  __ \\_   _|");
+        LOGGER.info("  | ' /| | ___ _ _ __        /  \\  | |__) || |  ");
+        LOGGER.info("  |  < | |/ _ \\ | '_ \\      / /\\ \\ |  ___/ | |  ");
+        LOGGER.info("  | . \\| |  __/ | | | |    / ____ \\| |    _| |_ ");
+        LOGGER.info("  |_|\\_\\_|\\___|_|_| |_|   /_/    \\_\\_|   |_____|");
+    }
+
+    void onStop(@Observes ShutdownEvent ev) {
+        LOGGER.info("The application VILLAIN is stopping...");
+    }
+}
